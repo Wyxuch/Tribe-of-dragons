66a7db12781fc5ba79b8dadb5eb79f16f2a513b0
wyxuch <stefankieubasa@gmail.com>
2020-01-29 18:15:40 +0100

    Fixed code added mistakenly

--- src/App.js
+++ src/App.js
@@ -24,32 +24,3 @@ function App() {
 }
 
 export default App;
-
-const clicker = {
-  time: 100,
-  targetX: 1,
-  targetY: 1,
-  work: true,
-  start: function(){
-    this.work = true
-    setInterval(()=>{
-
-      if(this.work){
-        for(let i = 0; i < 1999; i++){
-        document.elementFromPoint(this.targetX, this.targetY).click()
-      }
-      }else{ 
-        clearInterval(this.start)
-      }
-    
-    },this.time)
-  },
-  stop: function(){
-    this.work = false
-  }
-}
-document.getElementsByTagName('body')[0].onclick = function(e){
-  let x = e.pageX;
-  let y = e.pageY;
-  console.log(x, y)
-}
